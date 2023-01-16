39032f52ca889068b68cb3d8af52a01cee902356
Majorx234 <majorx234@googlemail.com>
2023-01-16 01:54:21 +0100

    Add stft lib to wrap rustfft and calc spectrum of left input (demo)

--- src/spectrogram_gui/bin/spectrogram_gui.rs
+++ src/spectrogram_gui/bin/spectrogram_gui.rs
@@ -1,6 +1,9 @@
 use eframe::egui;
 use ringbuf::Consumer;
 use ringbuf::SharedRb;
+use spectrogram_lib::stft;
+use spectrogram_lib::stft::WindowType;
+use spectrogram_lib::stft::STFT;
 use std::f32;
 use std::mem::MaybeUninit;
 use std::sync::Arc;
@@ -37,21 +40,23 @@ fn fm_data_signal_generator(num_samples: usize, phase_fm: f32) -> Vec<f32> {
     return values_data;
 }
 
-#[derive(PartialEq)]
+//#[derive(PartialEq)]
 struct Spectrum {
     values: Vec<f32>,
     window_size: usize,
     step_size: usize,
     time: f32,
+    stft: STFT<f32>,
 }
 
 impl Default for Spectrum {
     fn default() -> Self {
         Self {
             values: vec![0.0; 512],
-            window_size: 512,
-            step_size: 512,
+            window_size: 1024,
+            step_size: 1024,
             time: 0.0,
+            stft: STFT::new(WindowType::Hanning, 1024, 1024),
         }
     }
 }
@@ -125,23 +130,33 @@ impl eframe::App for SpectrogramGui {
             ui.vertical(|ui| {
                 match &mut self.ringbuffer_left_out {
                     Some(ringbuffer_left_out) => {
-                        while ringbuffer_left_out.len() >= self.spectrum.step_size {
+                        //while
+                        let mut tmp_vec: Vec<f32> = vec![0.0; 1024];
+                        if ringbuffer_left_out.len() >= self.spectrum.step_size {
                             let (older_audio, newer_audio) = ringbuffer_left_out.as_slices();
                             if older_audio.len() >= self.spectrum.step_size {
                                 for idx in (0..self.spectrum.step_size) {
-                                    self.spectrum.values[idx] = older_audio[idx];
+                                    tmp_vec[idx] = older_audio[idx];
                                 }
                             } else {
                                 for idx in 0..older_audio.len() {
-                                    self.spectrum.values[idx] = older_audio[idx];
+                                    tmp_vec[idx] = older_audio[idx];
                                 }
                                 for idx in older_audio.len()..self.spectrum.step_size {
-                                    self.spectrum.values[idx] =
-                                        newer_audio[idx - older_audio.len()];
+                                    tmp_vec[idx] = newer_audio[idx - older_audio.len()];
                                 }
                             }
                             ringbuffer_left_out.skip(self.spectrum.step_size);
+                            self.spectrum
+                                .stft
+                                .compute_column(&mut tmp_vec, &mut self.spectrum.values);
                         }
+                    }
+                    None => (),
+                }
+                match &mut self.ringbuffer_right_out {
+                    Some(ringbuffer_right_out) => {
+                        ringbuffer_right_out.skip(self.spectrum.step_size);
                     }
                     None => (),
                 }
