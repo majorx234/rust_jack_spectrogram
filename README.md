25693c9c3e02db9d4f8fed581f2db0dcbd60adb9
Majorx234 <majorx234@googlemail.com>
2023-01-20 00:24:43 +0100

    Update Readme, clean up code

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Info
-* WIP a GUI based dynamic spectrogram
+- WIP 
+- a GUI based dynamic spectrogram
+
+# ToDo
+- Spectrogram Widget in egui
+- improve fft call (less copy, use RealFFT-Crate)
+- interacting in GUI 
+
+# History
+- 2023-01-16 ploting spectrum 
+- 2023-01-16 ringbuffer is transporting data between threads 
--- src/spectrogram_gui/bin/spectrogram_gui.rs
+++ src/spectrogram_gui/bin/spectrogram_gui.rs
@@ -20,28 +20,6 @@ use plot::{
 
 type ConsumerRbf32 = Consumer<f32, Arc<SharedRb<f32, std::vec::Vec<MaybeUninit<f32>>>>>;
 
-fn fm_data_signal_generator(num_samples: usize, phase_fm: f32) -> Vec<f32> {
-    let freq: f32 = 1000.0;
-    let fsample_rate: f32 = 48000.0;
-    let phase: f32 = 0.0;
-    let modulator_hub: f32 = 400.0;
-    let modulator_freq: f32 = 10.0;
-    let modulator_index: f32 = modulator_hub / modulator_freq;
-    let shift = |t: f32, fmod: f32, fs: f32| -> f32 {
-        (2.0 * f32::consts::PI * t * fmod / fs + phase_fm).cos()
-    };
-    let values_data = (0..num_samples)
-        .map(|i| {
-            ((2.0 * f32::consts::PI * (freq / fsample_rate) * (i as f32)
-                + modulator_index * shift(i as f32, modulator_freq, fsample_rate)
-                + phase)
-                .sin())
-        })
-        .collect();
-
-    return values_data;
-}
-
 //#[derive(PartialEq)]
 struct Spectrum {
     values: Vec<f32>,
@@ -66,13 +44,10 @@ impl Default for Spectrum {
 impl Spectrum {
     fn ui(&mut self, ui: &mut Ui) -> Response {
         ui.ctx().request_repaint();
-        //self.time += ui.input().unstable_dt.at_most(1.0 / 30.0) as f32 * 0.1;
         self.bar_plot(ui)
     }
 
     fn bar_plot(&mut self, ui: &mut Ui) -> Response {
-        let phase_fm = self.time;
-        //self.values = fm_data_signal_generator(512, phase_fm);
         let mut chart = BarChart::new(
             (0..512)
                 .step_by(1)
@@ -89,7 +64,6 @@ impl Spectrum {
             .include_x(2.0)
             .include_y(512.0)
             .width(100.0)
-            //            .clamp_grid(true)
             .show(ui, |plot_ui| plot_ui.bar_chart(chart))
             .response
     }
@@ -115,6 +89,7 @@ impl Default for SpectrogramGui {
         }
     }
 }
+
 impl SpectrogramGui {
     pub fn set_ringbuffer(
         &mut self,
@@ -125,6 +100,7 @@ impl SpectrogramGui {
         self.ringbuffer_right_out = Some(ringbuffer_right_out);
     }
 }
+
 impl eframe::App for SpectrogramGui {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
