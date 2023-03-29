a3c1d9f46a3240489851a3a8a726c4a4b9fc4522
Majorx234 <majorx234@googlemail.com>
2023-03-29 04:03:26 +0200

    Exchange rustfft with realfft

--- src/stft.rs
+++ src/stft.rs
@@ -2,7 +2,7 @@ use apodize;
 use itertools::izip;
 use num::complex::Complex;
 use num::traits::{Float, Signed, Zero};
-use rustfft::{Fft, FftDirection, FftNum, FftPlanner};
+use realfft::{FftNum, RealFftPlanner, RealToComplex};
 use std::str::FromStr;
 use std::sync::Arc;
 
@@ -108,17 +108,14 @@ where
 {
     pub window_size: usize,
     pub step_size: usize,
-    pub fft: Arc<dyn Fft<T>>,
+    pub rfft: Arc<dyn RealToComplex<T>>,
     pub window: Option<Vec<T>>,
-    pub real_input: Vec<T>,
-    pub complex_input: Vec<Complex<T>>,
-    pub complex_output: Vec<Complex<T>>,
     pub scratch_space: Vec<Complex<T>>,
 }
 
 impl<T> STFT<T>
 where
-    T: FftNum + FromF64 + num::Float,
+    T: FftNum + FromF64 + num::Float + std::ops::MulAssign,
 {
     pub fn window_type_to_window_vec(
         window_type: WindowType,
@@ -164,30 +161,16 @@ where
         // window_size is power of two
         // step_size > 0
         assert!(step_size <= window_size);
-        let inverse = false;
-        let mut planner = FftPlanner::new(); //inverse);
-        let fft_dir: FftDirection = if inverse {
-            FftDirection::Inverse
-        } else {
-            FftDirection::Forward
-        };
-        let fft = planner.plan_fft(window_size, fft_dir);
-        let scratch_size = fft.get_inplace_scratch_len();
+        let mut real_planner = RealFftPlanner::<T>::new();
+        let rfft = real_planner.plan_fft_forward(window_size);
+        let scratch_space = rfft.make_scratch_vec();
+
         STFT {
             window_size: window_size,
             step_size: step_size,
-            fft: fft,
+            rfft: rfft,
             window: window,
-            real_input: std::iter::repeat(T::zero()).take(window_size).collect(),
-            complex_input: std::iter::repeat(Complex::<T>::zero())
-                .take(window_size)
-                .collect(),
-            complex_output: std::iter::repeat(Complex::<T>::zero())
-                .take(window_size)
-                .collect(),
-            scratch_space: std::iter::repeat(Complex::<T>::zero())
-                .take(scratch_size)
-                .collect(),
+            scratch_space: scratch_space,
         }
     }
 
@@ -196,53 +179,34 @@ where
         self.window_size / 2
     }
 
-    pub fn compute_into_complex_output(&mut self, input: &[T]) {
+    pub fn compute_into_complex_output(&mut self, input: &[T], output: &mut [Complex<T>]) {
         assert_eq!(input.len(), self.window_size);
 
         // multiply real_input with window
+        let mut real_input = self.rfft.make_input_vec();
         if let Some(ref window) = self.window {
-            // copy windowed input as real parts into complex_input
-            for (dst, src, window_elem) in
-                izip!(self.complex_input.iter_mut(), input.iter(), window.iter(),)
+            for (dst, src, window_elem) in izip!(real_input.iter_mut(), input.iter(), window.iter())
             {
-                dst.re = *src * *window_elem;
-                dst.im = T::zero();
+                *dst = *src * *window_elem;
             }
         } else {
-            // copy input as real parts into complex_input
-            for (dst, src) in self.complex_input.iter_mut().zip(input.iter()) {
-                dst.re = *src;
-                dst.im = T::zero();
+            for (dst, src) in real_input.iter_mut().zip(input.iter()) {
+                *dst = *src;
             }
-        }
-
+        };
         // compute fft
-        self.fft
-            .process_with_scratch(&mut self.complex_input, &mut self.scratch_space);
+        self.rfft
+            .process_with_scratch(&mut real_input, output, &mut self.scratch_space);
     }
 
     /// # Panics
     /// panics unless `self.output_size() == output.len()`
-    pub fn compute_complex_column(&mut self, input: &[T], output: &mut [Complex<T>]) {
+    pub fn compute_magnitude_column(&mut self, input: &mut [T], output: &mut [T]) {
         assert_eq!(self.output_size(), output.len());
+        let mut complex_output = self.rfft.make_output_vec();
+        self.compute_into_complex_output(input, &mut complex_output);
 
-        self.compute_into_complex_output(&input);
-
-        // copy inplace result of fft to output
-        for (dst, src) in output.iter_mut().zip(self.complex_input.iter()) {
-            *dst = src.clone();
-        }
-    }
-
-    /// # Panics
-    /// panics unless `self.output_size() == output.len()`
-    pub fn compute_magnitude_column(&mut self, input: &[T], output: &mut [T]) {
-        assert_eq!(self.output_size(), output.len());
-
-        self.compute_into_complex_output(&input);
-
-        // copy inplace result of fft to output
-        for (dst, src) in output.iter_mut().zip(self.complex_input.iter()) {
+        for (dst, src) in output.iter_mut().zip(complex_output.iter()) {
             *dst = src.norm();
         }
     }
@@ -253,10 +217,10 @@ where
     pub fn compute_column(&mut self, input: &[T], output: &mut [T]) {
         assert_eq!(self.output_size(), output.len());
 
-        self.compute_into_complex_output(input);
+        let mut complex_output = self.rfft.make_output_vec();
+        self.compute_into_complex_output(input, &mut complex_output);
 
-        // copy inplace result of fft to output
-        for (dst, src) in output.iter_mut().zip(self.complex_input.iter()) {
+        for (dst, src) in output.iter_mut().zip(complex_output.iter()) {
             *dst = log10_positive(src.norm());
         }
     }
