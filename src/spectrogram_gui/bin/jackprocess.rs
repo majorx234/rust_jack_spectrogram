63b040a575dbc7e539c1153934029d4dba272f90
MajorX234 <majorx234@googlemail.com>
2023-11-14 00:10:46 +0100

    Simplify iterator argument

--- src/spectrogram_gui/bin/jackprocess.rs
+++ src/spectrogram_gui/bin/jackprocess.rs
@@ -42,8 +42,8 @@ pub fn start_jack_thread(
             let in_a_p = in_a.as_slice(ps);
             let in_b_p = in_b.as_slice(ps);
 
-            ringbuffer_left_in.push_iter(&mut in_a_p.into_iter().map(|x| *x));
-            ringbuffer_right_in.push_iter(&mut in_b_p.into_iter().map(|x| *x));
+            ringbuffer_left_in.push_iter(&mut in_a_p.iter().copied());
+            ringbuffer_right_in.push_iter(&mut in_b_p.iter().copied());
             jack::Control::Continue
         };
         let process = jack::ClosureProcessHandler::new(process_callback);
